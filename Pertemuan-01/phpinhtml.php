c47bfde4c6a10347d96983e5b3f1e64298dfa09c
vyzz-xz <fizdstore@gmail.com>
2026-02-09 20:44:46 +0700

    Pertemuan 1 : Basic PHP

---
+++ Pertemuan-01/phpinhtml.php
@@ -0,0 +1,16 @@
+
+<?php
+    echo "Pemrograman Berbasis Web";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hallo <?php echo "World"; ?></h1>
+</body>
+</html>
---
+++ Pertemuan-01/variable.php
@@ -0,0 +1,16 @@
+
+<?php
+    // tipe data string
+    $nama = "Cecep";
+
+    // tipe data integer
+    $umur = 25;
+
+    // tipe data float
+    $berat = 70.5;
+
+    // Menampilkan variabel
+    echo "Nama: " . $nama . "<br>";
+    echo "Umur: " . $umur . " tahun<br>";
+    echo "Berat: " . $berat . " kg<br>";
+?>
