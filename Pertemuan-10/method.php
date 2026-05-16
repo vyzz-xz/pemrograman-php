a742c110e9712fc71897ff9fe3708da5289acc7f
vyzz-xz <fizdstore@gmail.com>
2026-05-16 21:22:59 +0700

    Pertemuan 10 : Tugas PHP Restful API

--- Pertemuan-10/method.php
+++ Pertemuan-10/method.php
@@ -31,20 +31,25 @@ class Produksi
     // Fungsi buat nampilin 1 data aja berdasarkan ID
     public function get_log($id = 0)
     {
+        // Buat akses ke koneksi db
         global $koneksi;
-        $query = "SELECT * FROM production_logs";
+        $query = "SELECT * FROM production_logs"; //Query buat ngambil data
         
+        //Buat kondisi kalau ID-nya nggak 0, berarti kita mau ngambil data berdasarkan ID tertentu
         if ($id != 0) {
             $query .= " WHERE id=" . $id . " LIMIT 1";
         }
         
+        // Array Kosong buat nampung data yang diambil
         $data = array();
         $result = $koneksi->query($query);
         
+        // Loop buat masukin data yang diambil ke array $data
         while ($row = mysqli_fetch_object($result)) {
             $data[] = $row;
         }
         
+        // Buat response dalam format JSON
         $response = array(
             'status' => 1,
             'message' => 'Get Production Log Successfully.',
@@ -58,6 +63,7 @@ class Produksi
     // Fungsi buat nambahin data log baru
     public function insert_log()
     {
+        // Kita cek dulu apakah data yang dikirim lewat POST itu lengkap atau nggak
         global $koneksi;
         $arrcheckpost = array(
             'kode_mesin' => '', 
@@ -66,8 +72,10 @@ class Produksi
             'jumlah_reject' => ''
         );
         
+        // Buat hitung berapa banyak data yang dikirim lewat POST
         $hitung = count(array_intersect_key($_POST, $arrcheckpost));
-        
+
+        // Kalau data yang dikirim lewat POST itu lengkap, kita masukin ke database
         if ($hitung == count($arrcheckpost)) {
             $result = mysqli_query($koneksi, "INSERT INTO production_logs SET
                 kode_mesin = '$_POST[kode_mesin]',
@@ -75,11 +83,13 @@ class Produksi
                 jumlah_lulus = '$_POST[jumlah_lulus]',
                 jumlah_reject = '$_POST[jumlah_reject]'");
             
+            // Kita cek apakah data berhasil masuk ke database atau nggak
             if ($result) {
                 $response = array('status' => 1, 'message' => 'Log Added Successfully.');
             } else {
                 $response = array('status' => 0, 'message' => 'Log Addition Failed.');
             }
+        // Kalau data yang dikirim lewat POST nggak lengkap, kita kasih response error
         } else {
             $response = array('status' => 0, 'message' => 'Parameter Do Not Match');
         }
@@ -99,8 +109,10 @@ class Produksi
             'jumlah_reject' => ''
         );
         
+        // Buat hitung berapa banyak data yang dikirim lewat POST
         $hitung = count(array_intersect_key($_POST, $arrcheckpost));
         
+        // Kalau data yang dikirim lewat POST itu lengkap, kita update data di database berdasarkan ID
         if ($hitung == count($arrcheckpost)) {
             $result = mysqli_query($koneksi, "UPDATE production_logs SET
                 kode_mesin = '$_POST[kode_mesin]',
@@ -109,11 +121,13 @@ class Produksi
                 jumlah_reject = '$_POST[jumlah_reject]'
                 WHERE id='$id'");
             
+            // Kita cek apakah data berhasil diupdate di database atau nggak
             if ($result) {
                 $response = array('status' => 1, 'message' => 'Log Updated Successfully.');
             } else {
                 $response = array('status' => 0, 'message' => 'Log Updation Failed.');
             }
+        // Kalau data yang dikirim lewat POST nggak lengkap, kita kasih response error
         } else {
             $response = array('status' => 0, 'message' => 'Parameter Do Not Match');
         }
@@ -124,16 +138,20 @@ class Produksi
 
     // Fungsi buat ngehapus data log
     function delete_log($id)
-    {
+    {   
+        // Buat akses ke koneksi db
         global $koneksi;
-        $query = "DELETE FROM production_logs WHERE id=" . $id;
+        $query = "DELETE FROM production_logs WHERE id=" . $id; //Query buat ngehapus data berdasarkan ID
         
+        // Kita cek apakah data berhasil dihapus dari database atau nggak
         if (mysqli_query($koneksi, $query)) {
             $response = array('status' => 1, 'message' => 'Log Deleted Successfully.');
+        // Kalau data gagal dihapus dari database, kita kasih response error
         } else {
             $response = array('status' => 0, 'message' => 'Log Deletion Failed.');
         }
         
+        //Buat Header dan nampilin response dalam format JSON
         header('Content-Type: application/json');
         echo json_encode($response);
     }
